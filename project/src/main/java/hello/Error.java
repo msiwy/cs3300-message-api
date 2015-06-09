43a1141a5d50cf36f42a477b36eb484d9c8b99fc
yunalee <movement0118@gmail.com>
2015-06-09 13:18:34 -0400

    Jesse's commit : tested adding another mapping for error handling

---
+++ project/src/main/java/hello/Error.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by gatsby on 6/4/15.
+ */
+public class Error {
+    private final long id;
+    private final String content;
+
+    public Error(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- project/src/main/java/hello/GreetingController.java
+++ project/src/main/java/hello/GreetingController.java
@@ -8,12 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Developers") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    private static final String template1 = "This is an error page, %s";
+    private final AtomicLong counter1 = new AtomicLong();
+
+    @RequestMapping("/error1")
+    public Error error(@RequestParam(value="id", defaultValue="default") String name) {
+        return new Error(counter1.incrementAndGet(),
+                String.format(template1, name));
+    }
+
+
 }
