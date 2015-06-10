e4e0913527a370e2f4054be826a70601eee978a6
Sam Ford <samford@lawn-143-215-56-81.lawn.gatech.edu>
2015-06-10 19:38:55 -0400

    Messages Commit

---
+++ project/src/main/java/hello/Message.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by samford on 6/10/15.
+ */
+public class Message {
+//    messageId: 100, dateCreated : 34623754762354, content : "Hey Doug", senderId : 1234
+    public Message() {
+
+    }
+}
---
+++ project/src/main/java/hello/MessageController.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by samford on 6/10/15.
+ */
+public class MessageController {
+
+    public
+    @Resquest Pa
+
+}
